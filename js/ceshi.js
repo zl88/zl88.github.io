5fab50ada51806f360f65370df0875b5b1dd71ba
zl88 <zhang1932@foxmail.com>
2023-10-27 21:13:37 +0800

    Site updated: 2023-10-27 21:13:14

--- js/ceshi.js
+++ js/ceshi.js
@@ -1,1 +1,190 @@
-!function(){"use strict";const e=document.createElement("img");e.src="https://cdn.h5ds.com/space/files/600972551685382144/20230826/619851245639618560.png",e.style.width="60px",e.style.height="60px",e.style.borderRadius="8px",e.style.marginBottom="10px";const t=[{text:"博客地址",url:"/",desc:"本博客的常用两个网址"},{text:"博客 1",url:"https://zl88.github.io",desc:"github部署的博客地址"},{text:"博客 2",url:"https://xhcy.4everland.app",desc:"博客4everland地址"},{text:"视传导航",url:"https://link3.cc/xiadj",desc:"夏dj导航网址发布页"},{text:"视界回眸",url:"/video/index.html",desc:"全能VIP视频在线解析"},{text:"每日一练",url:"/sa/index.html",desc:"好好学习，天天向上"},{text:"全部分类",url:"/categories/index.html",desc:"文章的分类"}],l=document.createElement("div");l.style.position="fixed",l.style.top="25%",l.style.right="-250px",l.style.backgroundColor="var(--card-bg)",l.style.padding="10px",l.style.border="1px solid var(--btn-color)",l.style.borderRadius="12px",l.style.boxShadow="var(--card-box-shadow)",l.style.display="none",l.style.zIndex="9999",l.style.transition="right 0.5s ease-in-out";const s=document.createElement("div");s.style.display="flex",s.style.flexDirection="column",s.style.alignItems="center",s.appendChild(e);for(const e of t){const t=document.createElement("a");t.href=e.url,t.textContent=e.text,t.target="_blank",t.style.color="var(--text-highlight-color)",t.style.display="flex",t.style.alignItems="center",t.style.lineHeight="2",t.addEventListener("mouseenter",(function(){t.style.color="var(--long-blue)"})),t.addEventListener("mouseleave",(function(){t.style.color=""})),t.style.position="relative";const l=document.createElement("span");l.textContent=e.desc,l.style.position="absolute",l.style.bottom="100%",l.style.right="calc(100% + 35px)",l.style.width="10rem",l.style.bottom="-25%",l.style.color="var(--text-highlight-color)",l.style.textAlign="left",l.style.background="var(--card-bg)",l.style.border="1px solid var(--btn-color)",l.style.boxShadow="var(--card-box-shadow)",l.style.padding="5px 5px 5px 10px",l.style.borderRadius="6px",l.style.visibility="hidden",l.style.opacity="0";const i=document.createElement("div");i.style.position="absolute",i.style.right="calc(100% + 24px)",i.style.border="solid",i.style.borderWidth="6px 0px 6px 12px",i.style.visibility="hidden",i.style.opacity="0",i.style.borderColor="transparent transparent transparent var(--btn-color)",t.appendChild(l),t.appendChild(i),t.addEventListener("mouseenter",(function(){l.style.visibility="visible",l.style.opacity="1",i.style.visibility="visible",i.style.opacity="1"})),t.addEventListener("mouseleave",(function(){l.style.visibility="hidden",l.style.opacity="0",i.style.visibility="hidden",i.style.opacity="0"})),s.appendChild(t)}let i;l.appendChild(s),document.body.appendChild(l);let o=!1;document.body.addEventListener("mousemove",(function(e){window.innerWidth-e.clientX<=10&&(clearTimeout(i),l.style.right="10px",l.style.display="block",i=setTimeout((function(){l.style.right="-250px",setTimeout((function(){l.style.display="none"}),500)}),3e3))})),l.addEventListener("mouseenter",(function(){o=!0}))}();
+//右侧导航
+
+(function () {
+    'use strict';
+    // 创建作者头像
+    const authorAvatar = document.createElement("img");
+    authorAvatar.src = "https://cdn.h5ds.com/space/files/600972551685382144/20230826/619851245639618560.png";
+    // authorAvatar.src = "https://thirdqq.qlogo.cn/g?b=sdk&k=yqjbmdENrko19T7mjIc3vg&s=100&t=1655292317";
+    authorAvatar.style.width = "60px";
+    authorAvatar.style.height = "60px";
+    authorAvatar.style.borderRadius = "8px"; // 设置圆角为8px
+    authorAvatar.style.marginBottom = "10px";
+
+    // 创建链接数组
+    const links = [
+        {
+            text: "博客地址",
+            url: `/`,
+            desc: "本博客的常用两个网址",
+        },
+        {
+            text: "博客 1",
+            url: `https://zl88.github.io`,
+            desc: "github部署的博客地址",
+        },
+        {
+            text: "博客 2",
+            url: `https://xhcy.4everland.app`,
+            desc: "博客4everland地址",
+        },
+        {
+            text: "视传导航",
+            url: `https://link3.cc/xiadj`,
+            desc: "夏dj导航网址发布页",
+        },
+        {
+            text: "视界回眸",
+            url: `/video/index.html`,
+            desc: "全能VIP视频在线解析",
+        },
+        {
+            text: "每日一练",
+            url: `/sa/index.html`,
+            desc: "好好学习，天天向上",
+        },
+        {
+            text: "全部分类",
+            url: `/categories/index.html`,
+            desc: "文章的分类",
+        },
+        // 添加更多链接对象
+    ];
+
+    // 创建弹窗容器
+    const popup = document.createElement("div");
+    popup.style.position = "fixed";
+    popup.style.top = "25%";
+    popup.style.right = "-250px"; // 初始位置在右侧屏幕外
+    popup.style.backgroundColor = "var(--card-bg)";
+    popup.style.padding = "10px";
+    popup.style.border = "1px solid var(--btn-color)";
+    popup.style.borderRadius = "12px";
+    popup.style.boxShadow = "var(--card-box-shadow)";
+    popup.style.display = "none";
+    popup.style.zIndex = "9999"; // 确保弹窗在最顶层
+    popup.style.transition = "right 0.5s ease-in-out"; // 添加过渡效果
+
+    // 创建内部容器
+    const innerContainer = document.createElement("div");
+    innerContainer.style.display = "flex";
+    innerContainer.style.flexDirection = "column";
+    innerContainer.style.alignItems = "center"; // 垂直居中
+
+    // 添加作者头像和链接到内部容器
+    innerContainer.appendChild(authorAvatar);
+
+    // 循环创建链接并添加到内部容器
+    for (const linkData of links) {
+        const link = document.createElement("a");
+        link.href = linkData.url;
+        link.textContent = linkData.text;
+        link.target = "_blank";
+        link.style.color = "var(--text-highlight-color)";
+        link.style.display = "flex";
+        link.style.alignItems = "center"; // 垂直居中
+        link.style.lineHeight = "2"; // 设置链接行高
+        link.addEventListener("mouseenter", function () {
+            link.style.color = "var(--long-blue)"; // 设置链接字体颜色
+        });
+        link.addEventListener("mouseleave", function () {
+            link.style.color = ""; // 恢复链接字体颜色
+        });
+
+        // 使用伪元素方式添加网站信息
+        link.style.position = "relative";
+
+        const descElement = document.createElement("span");
+        descElement.textContent = linkData.desc;
+        descElement.style.position = "absolute";
+        descElement.style.bottom = "100%"; // 上移
+        descElement.style.right = "calc(100% + 35px)"; // 右侧 5px 外
+        descElement.style.width = "10rem"; // 信息宽度
+        descElement.style.bottom = "-25%";
+        descElement.style.color = "var(--text-highlight-color)";
+        descElement.style.textAlign = "left";
+        descElement.style.background = "var(--card-bg)";
+        descElement.style.border = "1px solid var(--btn-color)";
+        descElement.style.boxShadow = "var(--card-box-shadow)";
+        descElement.style.padding = "5px 5px 5px 10px";
+        descElement.style.borderRadius = "6px";
+        descElement.style.visibility = "hidden";
+        descElement.style.opacity = "0";
+
+        // 添加聊天气泡的小三角
+        const triangle = document.createElement("div");
+        triangle.style.position = "absolute";
+        triangle.style.right = "calc(100% + 24px)"; // 右侧 10px 外
+        triangle.style.border = "solid";
+        triangle.style.borderWidth = "6px 0px 6px 12px";
+        triangle.style.visibility = "hidden";
+        triangle.style.opacity = "0";
+        triangle.style.borderColor = "transparent transparent transparent var(--btn-color)";
+
+        link.appendChild(descElement);
+        link.appendChild(triangle);
+
+        link.addEventListener("mouseenter", function () {
+            descElement.style.visibility = "visible";
+            descElement.style.opacity = "1";
+            triangle.style.visibility = "visible";
+            triangle.style.opacity = "1";
+        });
+
+        link.addEventListener("mouseleave", function () {
+            descElement.style.visibility = "hidden";
+            descElement.style.opacity = "0";
+            triangle.style.visibility = "hidden";
+            triangle.style.opacity = "0";
+        });
+
+        innerContainer.appendChild(link);
+    }
+
+    // 将内部容器添加到弹窗容器
+    popup.appendChild(innerContainer);
+
+    // 添加弹窗到页面
+    document.body.appendChild(popup);
+
+    let hoverTimeout;
+    let mouseInPopup = false; // 用于检测鼠标是否在弹窗内
+
+    // 鼠标移到网页右侧10px边缘显示弹窗
+    document.body.addEventListener("mousemove", function (event) {
+        // 获取页面宽度
+        var pageWidth = window.innerWidth;
+
+        // 判断鼠标距离右侧边缘的距离
+        if (pageWidth - event.clientX <= 10) {
+            clearTimeout(hoverTimeout);
+            popup.style.right = "10px"; // 从右向左滑出
+            popup.style.display = "block";
+            // 设置定时器，3秒后弹窗自自动消失
+            hoverTimeout = setTimeout(function () {
+                popup.style.right = "-250px"; // 从左向右滑出
+                setTimeout(function () {
+                    popup.style.display = "none";
+                }, 500); // 500毫秒后隐藏
+            }, 3000); // 3秒后自动隐藏
+        }
+    });
+
+    // 鼠标进入弹窗时更新标志
+    popup.addEventListener("mouseenter", function () {
+        mouseInPopup = true;
+    });
+
+    // 鼠标离开弹窗时更新标志
+    // popup.addEventListener("mouseleave", function () {
+    //     mouseInPopup = false;
+    //     // 检测鼠标是否还在弹窗外，如果不在则隐藏弹窗
+    //     if (!mouseInPopup) {
+    //         popup.style.right = "-250px"; // 从左向右滑出
+    //         setTimeout(function () {
+    //             popup.style.display = "none";
+    //         }, 1000); // 1秒后隐藏
+    //     }
+    // });
+})();
+
--- js/commentBarrage.js
+++ js/commentBarrage.js
@@ -1,1 +1,172 @@
-var hotreview={switchCommentBarrage:function(){document.querySelector(".comment-barrage")&&($(".comment-barrage").is(":visible")?($(".comment-barrage").hide(),$(".menu-commentBarrage-text").text("显示热评"),document.querySelector("#consoleCommentBarrage").classList.remove("on"),localStorage.setItem("commentBarrageSwitch","false")):$(".comment-barrage").is(":hidden")&&($(".comment-barrage").show(),$(".menu-commentBarrage-text").text("关闭热评"),document.querySelector("#consoleCommentBarrage").classList.add("on"),localStorage.removeItem("commentBarrageSwitch"))),rm.hideRightMenu()},scrollTo:function(e){const t=document.getElementById(e);if(t){const e=t.getBoundingClientRect().top+window.pageYOffset-80,r=window.pageYOffset,a=e-r;let n=null;window.requestAnimationFrame((function e(t){n||(n=t);const o=t-n,m=(c=Math.min(o/0,1))<.5?2*c*c:(4-2*c)*c-1;var c;window.scrollTo(0,r+a*m),o<600&&window.requestAnimationFrame(e)}))}}},postCommentElement=document.getElementById("post-comment");if(postCommentElement){var commentBarrageConfig={maxBarrage:1,barrageTime:4e3,twikooUrl:"https://twikoo.jessaybluck.cf",accessToken:"prj_92SfgsNRPbXIVUmT91AUBk1b6sX3",pageUrl:window.location.pathname,barrageTimer:[],barrageList:[],barrageIndex:0,dom:document.querySelector(".comment-barrage")},commentInterval=null,hoverOnCommentBarrage=!1;function initCommentBarrage(){var e=JSON.stringify({event:"COMMENT_GET","commentBarrageConfig.accessToken":commentBarrageConfig.accessToken,url:commentBarrageConfig.pageUrl}),t=new XMLHttpRequest;t.withCredentials=!0,t.addEventListener("readystatechange",(function(){4===this.readyState&&(commentBarrageConfig.barrageList=commentLinkFilter(JSON.parse(this.responseText).data),commentBarrageConfig.dom.innerHTML="")})),t.open("POST",commentBarrageConfig.twikooUrl),t.setRequestHeader("Content-Type","application/json"),t.send(e),clearInterval(commentInterval),commentInterval=null,commentInterval=setInterval((()=>{commentBarrageConfig.barrageList.length&&!hoverOnCommentBarrage&&(popCommentBarrage(commentBarrageConfig.barrageList[commentBarrageConfig.barrageIndex]),commentBarrageConfig.barrageIndex+=1,commentBarrageConfig.barrageIndex%=commentBarrageConfig.barrageList.length),commentBarrageConfig.barrageTimer.length>(commentBarrageConfig.barrageList.length>commentBarrageConfig.maxBarrage?commentBarrageConfig.maxBarrage:commentBarrageConfig.barrageList.length)&&!hoverOnCommentBarrage&&removeCommentBarrage(commentBarrageConfig.barrageTimer.shift())}),commentBarrageConfig.barrageTime)}function commentLinkFilter(e){e.sort(((e,t)=>e.created-t.created));let t=[];return e.forEach((e=>{t.push(...getCommentReplies(e))})),t}function getCommentReplies(e){if(e.replies){let t=[e];return e.replies.forEach((e=>{t.push(...getCommentReplies(e))})),t}return[]}function popCommentBarrage(e){let t=document.createElement("div");commentBarrageConfig.dom.clientWidth,commentBarrageConfig.dom.clientHeight;t.className="comment-barrage-item",t.innerHTML=`\n\t\t<div class="barrageHead">\n      \t\t<a class="barrageTitle" href="javascript:hotreview.scrollTo('post-comment')"">热评</a>\n\t\t\t<div class="barrageNick">${e.nick}</div>\n\t\t\t<img class="barrageAvatar" src="https://cravatar.cn/avatar/${e.mailMd5}"/>\n\t\t\t<a class="comment-barrage-close" href="javascript:hotreview.switchCommentBarrage()"><i class="iconfont icat-close"></i></a>\n\t\t</div>\n\t\t<a class="barrageContent" href="javascript:hotreview.scrollTo('${e.id}');">${e.comment}</a>\n\t`,commentBarrageConfig.barrageTimer.push(t),commentBarrageConfig.dom.append(t)}function removeCommentBarrage(e){e.className="comment-barrage-item out",setTimeout((()=>{commentBarrageConfig.dom.removeChild(e)}),1e3)}$(".comment-barrage").hover((function(){hoverOnCommentBarrage=!0,console.log("热评悬浮")}),(function(){hoverOnCommentBarrage=!1,console.log("停止悬浮")})),document.addEventListener("scroll",btf.throttle((function(){var e=window.scrollY+document.documentElement.clientHeight,t=(window.scrollY,document.querySelector(".comment-barrage")),r=document.getElementById("post-comment");if(r&&t){var a=r.offsetTop+r.offsetHeight/2;document.body.clientWidth>768&&(t.style.bottom=a>e?"40px":"-200px")}}),200)),initCommentBarrage(),document.addEventListener("pjax:send",(function(){clearInterval(commentInterval)}))}else console.log("小张提醒您：此页面没有评论！");
+var hotreview = {
+    switchCommentBarrage: function () {
+        document.querySelector(".comment-barrage") && ($(".comment-barrage").is(":visible") ? ($(".comment-barrage").hide(), $(".menu-commentBarrage-text").text("显示热评"), document.querySelector("#consoleCommentBarrage").classList.remove("on"), localStorage.setItem("commentBarrageSwitch", "false")) : $(".comment-barrage").is(":hidden") && ($(".comment-barrage").show(), $(".menu-commentBarrage-text").text("关闭热评"), document.querySelector("#consoleCommentBarrage").classList.add("on"), localStorage.removeItem("commentBarrageSwitch"))),
+            rm.hideRightMenu()
+    },
+    scrollTo: function (e) {
+        const t = document.getElementById(e);
+        if (t) {
+            const e = t.getBoundingClientRect().top + window.pageYOffset - 80,
+                o = window.pageYOffset,
+                n = e - o;
+            let a = null;
+            window.requestAnimationFrame((function e(t) {
+                a || (a = t);
+                const i = t - a,
+                    l = (c = Math.min(i / 0, 1)) < .5 ? 2 * c * c : (4 - 2 * c) * c - 1;
+                var c;
+                window.scrollTo(0, o + n * l),
+                    i < 600 && window.requestAnimationFrame(e)
+            }))
+        }
+    },
+}
+// 热评弹窗跳转
+
+
+
+var postCommentElement = document.getElementById("post-comment");
+if (postCommentElement) {
+    var commentBarrageConfig = {
+        //同时最多显示弹幕数
+        maxBarrage: 1,
+        //弹幕显示间隔时间ms
+        barrageTime: 4000,
+        //twikoo部署地址腾讯云的为环境ID  vercel为域名
+        twikooUrl: "https://twikoo.jessaybluck.cf",
+        //token获取见上方 https://vercel.com/xiaolongmr/hexo-twikoo/settings/general  Project ID
+        accessToken: "prj_92SfgsNRPbXIVUmT91AUBk1b6sX3",
+        pageUrl: window.location.pathname,
+        barrageTimer: [],
+        barrageList: [],
+        barrageIndex: 0,
+        dom: document.querySelector('.comment-barrage'),
+    }
+
+    var commentInterval = null;
+    var hoverOnCommentBarrage = false;
+
+    $(".comment-barrage").hover(function () {
+        hoverOnCommentBarrage = true;
+        console.log("热评悬浮");
+    }, function () {
+        hoverOnCommentBarrage = false;
+        console.log("停止悬浮");
+    });
+
+    function initCommentBarrage() {
+        // console.log("开始创建热评")
+
+        var data = JSON.stringify({
+            "event": "COMMENT_GET",
+            "commentBarrageConfig.accessToken": commentBarrageConfig.accessToken,
+            "url": commentBarrageConfig.pageUrl
+        });
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                commentBarrageConfig.barrageList = commentLinkFilter(JSON.parse(this.responseText).data);
+                commentBarrageConfig.dom.innerHTML = '';
+            }
+        });
+        xhr.open("POST", commentBarrageConfig.twikooUrl);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(data);
+
+
+        clearInterval(commentInterval);
+        commentInterval = null;
+
+        commentInterval = setInterval(() => {
+            if (commentBarrageConfig.barrageList.length && !hoverOnCommentBarrage) {
+                popCommentBarrage(commentBarrageConfig.barrageList[commentBarrageConfig.barrageIndex]);
+                commentBarrageConfig.barrageIndex += 1;
+                commentBarrageConfig.barrageIndex %= commentBarrageConfig.barrageList.length;
+            }
+            if ((commentBarrageConfig.barrageTimer.length > (commentBarrageConfig.barrageList.length > commentBarrageConfig.maxBarrage ? commentBarrageConfig.maxBarrage : commentBarrageConfig.barrageList.length)) && !hoverOnCommentBarrage) {
+                removeCommentBarrage(commentBarrageConfig.barrageTimer.shift())
+            }
+        }, commentBarrageConfig.barrageTime)
+    }
+    function commentLinkFilter(data) {
+        data.sort((a, b) => {
+            return a.created - b.created;
+        })
+        let newData = [];
+        data.forEach(item => {
+            newData.push(...getCommentReplies(item));
+        });
+        return newData;
+    }
+    function getCommentReplies(item) {
+        if (item.replies) {
+            let replies = [item];
+            item.replies.forEach(item => {
+                replies.push(...getCommentReplies(item));
+            })
+            return replies;
+        } else {
+            return [];
+        }
+    }
+    function popCommentBarrage(data) {
+        let barrage = document.createElement('div');
+        let width = commentBarrageConfig.dom.clientWidth;
+        let height = commentBarrageConfig.dom.clientHeight;
+        barrage.className = 'comment-barrage-item'
+        barrage.innerHTML = `
+		<div class="barrageHead">
+      		<a class="barrageTitle" href="javascript:hotreview.scrollTo('post-comment')"">热评</a>
+			<div class="barrageNick">${data.nick}</div>
+			<img class="barrageAvatar" src="https://cravatar.cn/avatar/${data.mailMd5}"/>
+			<a class="comment-barrage-close" href="javascript:hotreview.switchCommentBarrage()"><i class="iconfont icat-close"></i></a>
+		</div>
+		<a class="barrageContent" href="javascript:hotreview.scrollTo('${data.id}');">${data.comment}</a>
+	`
+        commentBarrageConfig.barrageTimer.push(barrage);
+        commentBarrageConfig.dom.append(barrage);
+    }
+    function removeCommentBarrage(barrage) {
+        barrage.className = 'comment-barrage-item out';
+        setTimeout(() => {
+            commentBarrageConfig.dom.removeChild(barrage);
+        }, 1000)
+    }
+
+
+
+    // 自动隐藏
+    document.addEventListener('scroll', btf.throttle(function () {
+        //滚动条高度+视窗高度 = 可见区域底部高度
+        var visibleBottom = window.scrollY + document.documentElement.clientHeight;
+        //可见区域顶部高度
+        var visibleTop = window.scrollY;
+        // 获取翻页按钮容器
+        var pagination = document.querySelector('.comment-barrage');
+        // 获取位置监测容器，此处采用评论区
+        var eventlistner = document.getElementById('post-comment');
+        if (eventlistner && pagination) {
+            var centerY = eventlistner.offsetTop + (eventlistner.offsetHeight / 2);
+            if (document.body.clientWidth > 768) {
+                if (centerY > visibleBottom) {
+                    pagination.style.bottom = '40px';
+                } else {
+                    pagination.style.bottom = '-200px';
+                }
+            }
+        }
+    }, 200))
+
+    initCommentBarrage();
+
+    document.addEventListener('pjax:send', function () {
+        clearInterval(commentInterval);
+    });
+
+} else {
+    console.log("小张提醒您：此页面没有评论！");
+}
+
+// 热评弹窗函数
+
